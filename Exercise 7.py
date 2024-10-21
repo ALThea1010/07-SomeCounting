8dd21f93d1773bd1baf0973e78eba09581aec316
ALThea1010 <164994153+ALThea1010@users.noreply.github.com>
2024-10-21 12:09:57 +0400

    Add files via upload

---
+++ Exercise 7.py
@@ -0,0 +1,37 @@
+"""
+Assessment1-Programming-Skills-Portfolio Exercise 7: Some Counting
+"""
+
+
+# Loop that counts up from 0 to 50
+print("Counting up from 0 to 50")  # Description of the loop
+for number in range(0, 51, 1):  # Loop from 0 to 50 (inclusive)
+    print(number)  # Output the current number
+
+print("\n")  # Newline for separation between outputs
+
+# Loop that counts down from 50 to 0
+print("Countdown from 50 to 0")  # Description of the countdown
+for number in range(50, -1, -1):  # Loop from 50 to 0 (inclusive)
+    print(number)  # Output the current number
+
+print("\n")  # Newline for separation between outputs
+
+# Loop that counts up from 30 to 50
+print("Counting up from 30 to 50")  # Description of the loop
+for number in range(30, 51, 1):  # Loop from 30 to 50 (inclusive)
+    print(number)  # Output the current number
+
+print("\n")  # Newline for separation between outputs
+
+# Loop that counts down from 50 to 10
+print("Countdown from 50 to 10")  # Description of the countdown
+for number in range(50, 9, -2):  # Loop from 50 to 10 (exclusive), decrement by 2
+    print(number)  # Output the current number
+
+print("\n")  # Newline for separation between outputs
+
+# Loop that counts up from 100 to 200
+print("Counting up from 100 to 200")  # Description of the loop
+for number in range(100, 201, 5):  # Loop from 100 to 200 (inclusive), increment by 5
+    print(number)  # Output the current number
